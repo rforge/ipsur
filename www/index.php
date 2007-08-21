d08c9937140781abd79817a5371f459295c2c498
gkerns <gkerns@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-08-21 03:49:06 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,51 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<title>IPSUR Project</title>
+<link rel="Shortcut Icon" href="favicon.ico">
+<style type="text/css">
+<!--
+@import url("main.css");
+body {
+	background-image: url();
+}
 
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
-
-<?php
-
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
-
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
-
-  <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-  </head>
+-->
+</style>
+</head>
 
 <body>
+<div align="center">
 
-<! --- R-Forge Logo --- >
-<table border="0" width="100%" cellspacing="0" cellpadding="0">
-<tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+<table width="700" height="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td align="left" valign="top" bgcolor="#FFFFFF"><table width="700" height="140" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td width="200" align="center" valign="middle"><a href="index.php"><img border="0" src="resources/ipsur(main).jpg" alt="IPSUR" width="200" height="100" /></a></td>
+        <td width="500" height="120" align="center" valign="middle">
+          <p><a href="index.php"><img src="resources/title(main).jpg" alt="Introduction to Probability and Statistics Using R" width="500" height="100" border="0" /></a>G. Jay Kerns, Youngstown State University<br />
+          G. Andy Chang, Youngstown State University </p></td>
+      </tr>
+    </table>
+	<div id="navMenu">
+  <table width="700" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td width="175" align="center" valign="middle"><div align="center"><a href="index.htm">Home</a></div></td>
+      <td width="175" align="center" valign="middle"><div align="center"><a href="description.php">Description</a></div></td>
+      <td width="175" align="center" valign="middle"><div align="center"><a href="features.php">Features</a></div></td>
+	  <td width="175" align="center" valign="middle"><div align="center"><a href="news.php">What's New </a></div></td>
+    </tr>
+    <tr>
+      <td width="175" align="center" valign="middle"><div align="center"><a href="installation.php">Installation</a></div></td>
+      <td width="175" align="center" valign="middle"><div align="center"><a href="downloads.php">Downloads</a></div></td>
+      <td width="175" align="center" valign="middle"><div align="center"><a href="screens.php">Screen Shots </a></div></td>
+	  <td width="175" align="center" valign="middle"><div align="center"><a href="links.php">Links</a></div></td>
+    </tr>
+  </table>
+</div>
+
+	
+      <p class="articleTitle">What IPSUR Does:</p>
+      <blockquote>
+        <p> This package accompanies the text <em>Introduction to Probability and Statistics Using R</em>  (in progress) by G. Andy Chang and G. Jay Kerns. The package contributes functions unique to the book as well as specific configuration and selected functionality to R Commander, the excellent pioneering work by John Fox of McMaster University.</p>
+        <p> <span class="name">IPSUR</span>'s primary goal is to provide a user-friendly graphical user interface (GUI) to the open-source and freely available R statistical computing environment.  <span class="name">IPSUR</span> is equipped to handle many of the statistical analyses and graphical displays usually encountered by upper division undergraduate Mathematics, Statistics, and Engineering majors.  Available features are comparable to many expensive commercial packages such as<span class="name"> Minitab</span>, <span class="name">SPSS</span>, and <span class="name">JMP-IN</span>.</p>
+        <p> Since the audience of <span class="name">IPSUR</span> is slightly different than <span class="name">Rcmdr</span>'s, certain functionality has been added and selected error-checks have been disabled to permit the student to explore alternative regions of the statistical landscape. The resulting benefit of increased flexibility is balanced by somewhat increased vulnerability to syntax errors and misuse; the instructor should keep this and the academic audience in mind when considering <span class="name">IPSUR</span> for classroom adoption. </p>
+        <p> Comments and suggestions are welcomed.</p>
+        <p>&nbsp;</p>
+      </blockquote>
+      <p class="articleTitle">IPSUR Textbook Links (In Progress)</p>
+	  <div id="navMenu">
+      <table width="700" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+		  <td width="175" align="center" valign="middle"><div align="center"><a href="../index.htm">Home</a></div></td>	
+          <td width="175" align="center" valign="middle"><div align="center"><a href="../changes/index.htm">Changes</a></div></td>
+          <td width="175" align="center" valign="middle"><div align="center"><a href="../errata/index.htm">Errata</a></div></td>
+          <td width="175" align="center" valign="middle"><div align="center"><a href="../exercises/index.htm">Exercises</a></div></td>
+        </tr>
+      </table> </div>     <p>&nbsp;</p>
+      <p class="articleTitle">Contact Information </p>
+      <blockquote>
+        <p> <span class="name">IPSUR</span> comments, suggestions, bugs:   <a href="mailto:ipsurweb@gmail.com">ipsurweb@gmail.com</a><br />
+G. Jay Kerns: <a href="mailto:gkerns@ysu.edu">gkerns@ysu.edu</a> (<a href="http://www.cc.ysu.edu/~gjkerns">home page</a>)<br />
+G. Andy Chang: <a href="mailto:gchang@ysu.edu">gchang@ysu.edu</a> (<a href="http://www.cc.ysu.edu/~ghchang/">home page</a>)</p>
+        <p>&nbsp;</p>
+      </blockquote>
+	  <p align="center">
+	  (C) R Foundation, from http://www.r-project.org
+	  <p align="center"><!-- Start of StatCounter Code -->
+<script type="text/javascript" language="javascript">
+var sc_project=1820586; 
+var sc_invisible=0; 
+var sc_partition=13; 
+var sc_security="c80f8f9a"; 
+</script>
+
+<script type="text/javascript" language="javascript" src="http://www.statcounter.com/counter/counter.js"></script><noscript><a href="http://www.statcounter.com/" target="_blank"></a> 
+</noscript>
+<!-- End of StatCounter Code -->
+	  
+	  </td>
+  </tr>
 </table>
 
-
-<!-- get project title  -->
-<!-- own website starts here, the following may be changed as you like -->
-
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
-
-<!-- end of project description -->
-
-<p> No content added. </p>
-
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
-
+</div>
 </body>
 </html>
