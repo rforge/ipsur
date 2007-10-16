208bcc452c780fc07be6ea79f57d5515e89bc14e
bowsky <bowsky@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-10-16 14:39:16 +0000

    Website up to date, removed index.htm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,49 +1,39 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-<title>IPSUR Project</title>
-<link rel="Shortcut Icon" href="favicon.ico">
-<style type="text/css">
-<!--
-@import url("main.css");
-body {
-	background-image: url();
-}
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+	<title>IPSUR Project</title>
+	<link rel="Shortcut Icon" href="favicon.ico">
 
--->
-</style>
+	<style type="text/css">
+	<!--
+		@import url("main.css");
+		body {
+			background-image: url();
+		}
+	-->
+	</style>
 </head>
 
 <body>
-<div align="center">
+	<div align="center">
+	
+	<table width="700" height="100%" border="0" cellpadding="0" cellspacing="0">
+  		<tr>
+    		<td align="left" valign="top" bgcolor="#FFFFFF">
+			<!-- All of the page content is in this table-->
 
-<table width="700" height="100%" border="0" cellpadding="0" cellspacing="0">
-  <tr>
-    <td align="left" valign="top" bgcolor="#FFFFFF"><table width="700" height="140" border="0" cellpadding="0" cellspacing="0">
-      <tr>
-        <td width="200" align="center" valign="middle"><a href="index.php"><img border="0" src="resources/ipsur(main).jpg" alt="IPSUR" width="200" height="100" /></a></td>
-        <td width="500" height="120" align="center" valign="middle">
-          <p><a href="index.php"><img src="resources/title(main).jpg" alt="Introduction to Probability and Statistics Using R" width="500" height="100" border="0" /></a>G. Jay Kerns, Youngstown State University<br />
-          G. Andy Chang, Youngstown State University </p></td>
-      </tr>
-    </table>
-	<div id="navMenu">
-  <table width="700" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-      <td width="175" align="center" valign="middle"><div align="center"><a href="index.htm">Home</a></div></td>
-      <td width="175" align="center" valign="middle"><div align="center"><a href="description.php">Description</a></div></td>
-      <td width="175" align="center" valign="middle"><div align="center"><a href="features.php">Features</a></div></td>
-	  <td width="175" align="center" valign="middle"><div align="center"><a href="news.php">What's New </a></div></td>
-    </tr>
-    <tr>
-      <td width="175" align="center" valign="middle"><div align="center"><a href="installation.php">Installation</a></div></td>
-      <td width="175" align="center" valign="middle"><div align="center"><a href="downloads.php">Downloads</a></div></td>
-      <td width="175" align="center" valign="middle"><div align="center"><a href="screens.php">Screen Shots </a></div></td>
-	  <td width="175" align="center" valign="middle"><div align="center"><a href="links.php">Links</a></div></td>
-    </tr>
-  </table>
-</div>
+<!--BEGIN: Page Banner-->
+<?php
+	include "banner.php";
+?>
+<!--END: Page Banner-->
+	
+<!--BEGIN: Top Menu-->
+<?php
+	include "topMenu.php";
+?>
+<!--END: Top Menu-->
 
 	
       <p class="articleTitle">What IPSUR Does:</p>
@@ -58,15 +48,15 @@ body {
 	  <div id="navMenu">
       <table width="700" border="0" cellspacing="0" cellpadding="0">
         <tr>
-		  <td width="175" align="center" valign="middle"><div align="center"><a href="../index.htm">Home</a></div></td>	
-          <td width="175" align="center" valign="middle"><div align="center"><a href="../changes/index.htm">Changes</a></div></td>
-          <td width="175" align="center" valign="middle"><div align="center"><a href="../errata/index.htm">Errata</a></div></td>
-          <td width="175" align="center" valign="middle"><div align="center"><a href="../exercises/index.htm">Exercises</a></div></td>
+		  <td width="175" align="center" valign="middle"><div align="center"><a href="http://cc.ysu.edu/~gjkerns/IPSUR/index.htm">Home</a></div></td>	
+          <td width="175" align="center" valign="middle"><div align="center"><a href="http://cc.ysu.edu/~gjkerns/IPSUR/changes/index.htm">Changes</a></div></td>
+          <td width="175" align="center" valign="middle"><div align="center"><a href="http://cc.ysu.edu/~gjkerns/IPSUR/errata/index.htm">Errata</a></div></td>
+          <td width="175" align="center" valign="middle"><div align="center"><a href="http://cc.ysu.edu/~gjkerns/IPSUR/exercises/index.htm">Exercises</a></div></td>
         </tr>
       </table> </div>     <p>&nbsp;</p>
       <p class="articleTitle">Contact Information </p>
       <blockquote>
-        <p> <span class="name">IPSUR</span> comments, suggestions, bugs:   <a href="mailto:ipsurweb@gmail.com">ipsurweb@gmail.com</a><br />
+        <p> <span class="name">IPSUR</span> comments, suggestions, bugs:   <a href="mailto:ipsur-help@list.r-forge.org">ipsur-help@list.r-forge.org</a><br />
 G. Jay Kerns: <a href="mailto:gkerns@ysu.edu">gkerns@ysu.edu</a> (<a href="http://www.cc.ysu.edu/~gjkerns">home page</a>)<br />
 G. Andy Chang: <a href="mailto:gchang@ysu.edu">gchang@ysu.edu</a> (<a href="http://www.cc.ysu.edu/~ghchang/">home page</a>)</p>
         <p>&nbsp;</p>
--- www/menu.php
+++ www/menu.php
@@ -1,7 +1,7 @@
 <div id="navMenu">
   <table width="700" border="0" cellspacing="0" cellpadding="0">
     <tr>
-      <td width="175" align="center" valign="middle"><div align="center"><a href="index.htm">Home</a></div></td>
+      <td width="175" align="center" valign="middle"><div align="center"><a href="index.php">Home</a></div></td>
       <td width="175" align="center" valign="middle"><div align="center"><a href="description.php">Description</a></div></td>
       <td width="175" align="center" valign="middle"><div align="center"><a href="features.php">Features</a></div></td>
 	  <td width="175" align="center" valign="middle"><div align="center"><a href="news.php">What's New </a></div></td>
