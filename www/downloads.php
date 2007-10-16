43eb5a8910515dcedc477fa3465602e792295103
bowsky <bowsky@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-10-16 02:24:37 +0000

    Test Update - No real changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/downloads.php
+++ www/downloads.php
@@ -1,89 +1,148 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-<title>IPSUR Project - Downloads</title>
-<link rel="Shortcut Icon" href="favicon.ico">
-<style type="text/css">
-<!--
-@import url("main.css");
-body {
-	background-image: url();
-}
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+	<title>IPSUR Project</title>
+	<link rel="Shortcut Icon" href="favicon.ico">
 
--->
-</style>
+	<style type="text/css">
+	<!--
+		@import url("main.css");
+		body {
+			background-image: url();
+		}
+	-->
+	</style>
 </head>
 
 <body>
-<div align="center">
+	<div align="center">
+	
+	<table width="700" height="100%" border="0" cellpadding="0" cellspacing="0">
+  		<tr>
+    		<td align="left" valign="top" bgcolor="#FFFFFF">
+			<!-- All of the page content is in this table-->
 
-<table width="700" height="100%" border="0" cellpadding="0" cellspacing="0">
-  <tr>
-    <td align="left" valign="top" bgcolor="#FFFFFF"><table width="700" height="140" border="0" cellpadding="0" cellspacing="0">
-      <tr>
-        <td width="200" align="center" valign="middle"><a href="index.php"><img border="0" src="resources/ipsur(main).jpg" alt="IPSUR" width="200" height="100" /></a></td>
-        <td width="500" height="120" align="center" valign="middle">
-          <p><a href="index.php"><img src="resources/title(main).jpg" alt="Introduction to Probability and Statistics Using R" width="500" height="100" border="0" /></a>G. Jay Kerns, Youngstown State University<br />Dr. G. Andy Chang, Youngstown State University </p></td>
-      </tr>
-    </table>
-	<?php
-		include "menu.php";
-	?>
+<!--BEGIN: Page Banner-->
+<?php
+	include "banner.php";
+?>
+<!--END: Page Banner-->
 	
-      <p class="articleTitle">Download the IPSUR Package </p>
-      <blockquote>
-        <p>The current released version of the IPSUR package is 0.1-2. Currently the binary packages are only available for computers running Windows and MacOS X. 
-        <div align="center"><strong>IPSUR Released Version 0.1-2 </strong><br />
-            <a href="download/IPSUR/0.1-2/Windows-Binary/IPSUR_0.1-2.zip">Windows IPSUR Package</a><br />
-          <a href="download/IPSUR/0.1-2/MacOSX-Binary/IPSUR_0.1-2.tgz">MacOS X IPSUR Package</a></div>
-        <p>The current developmental version of the IPSUR package is 0.1-3. Currently the binary package is only available for computers running Windows. This contains very early code for new features and is not suitable for production environments. The developmental version should be used for testing purposes only. </p>
-        <div align="center"><strong>IPSUR Development Version 0.1-3 </strong><br />
-          <a href="download/IPSUR/0.1-3/Windows-Binary/IPSUR_0.1-3.zip">Windows IPSUR Package</a></div>
-        <p>The previous version of the IPSUR package is 0.1-1. The binary package is only available for computers running Windows. While this package will be available, it is recommended that you install the latest released version of the IPSUR package for maximum stability, speed, and features. 
-        <div align="center"><strong>IPSUR Previous Version 0.1-1 </strong><br />
-          <a href="download/IPSUR/0.1-1/Windows-Binary/IPSUR_0.1-1.zip">Windows IPSUR Package</a></div>
-        <p>To install the <span class="name">IPSUR</span> binary package, click on the appropriate link below to download the file. Save the package to a convenient location. In Windows:
-        <ol type="1">
-			  <li>Open <span class="name">R</span></li>
-				<li>Select <span class="name">Packages</span></li>
-				<li>Select <span class="name">Install package(s) from local zip files...</span></li>
-				<li>Browse and select the downloaded <span class="name">IPSUR</span> package</li>
-		  </ol>
-		In MacOS X:
-			<ol type="1">
-				<li>Open <span class="name">R</span></li>
-				<li>Select <span class="name">Packages &amp; data</span></li>
-				<li>Select <span class="name">Package Installer</span></li>
-				<li>From the Package Repository drop down menu select <span class="name">Local Binary Package</span></li>
-				<li>Click the <span class="name">Install</span> button below</li>
-				<li>Browse and select the downloaded <span class="name">IPSUR</span> package</li>
-		  </ol>
-	    </blockquote>
+<!--BEGIN: Top Menu-->
+<?php
+	include "topMenu.php";
+?>
+<!--END: Top Menu-->
 	
+<!--BEGIN: Page Content-->
+<p class="articleTitle">Download the IPSUR Package</p>
+<blockquote>
+<p>
+	The current released version of the IPSUR package is 0.1-2. Currently the binary packages are only available for computers running Windows and MacOS X. 
         
-        <div class="articleTitle">Download the IPSUR Source Package</div>
-		<blockquote>
-		  <p>The source code for the <span class="name">IPSUR</span> package is available under the newest version of the GPL.  The source code is recommended for platforms for which package binaries are not made available. It can also be used by developers on all platforms to modify the IPSUR package under the constraints of the GPL. </p>
-		  <p>The current version of the <span class="name">IPSUR</span> package source is 0.1-2 and is the recommended version. If you are a developer interested in the absolute newest version of the <span class="name">IPSUR</span> packed source is 0.1-3. This package should be considered only be used for developmental purposes. Finally, the previous <span class="name">IPSUR</span> package source, 0.1-1, is also available for those who need it. </p>
-		  <div align="center">
-		    <p><strong>IPSUR Released Version 0.1-2 Source </strong><br />
-	        <a href="download/IPSUR/0.1-2/source/IPSUR_0.1-2.tar.gz"> IPSUR Package Source </a></p>
-		    <p><strong>IPSUR Development Version 0.1-3 Source </strong><br />
-              <a href="download/IPSUR/0.1-3/source/IPSUR_0.1-3.tar.gz"> IPSUR Package Source </a></p>
-		    <p><strong>IPSUR Previous Version 0.1-2 Source </strong><br />
-              <a href="download/IPSUR/0.1-1/source/IPSUR_0.1-1.tar.gz"> IPSUR Package Source </a></p>
-		  </div>
-		  <p>Instructions to install from local source code will be coming soon. </p>
-		  <p>&nbsp;</p>
-		</blockquote>
-		 <p align="center">
-	  (C) R Foundation, from http://www.r-project.org
-	  <p align="center">
-	  </td>
-  </tr>
-</table>
+	<div align="center">
+		<strong>IPSUR Released Version 0.1-2 </strong>
+		<br /><a href="download/IPSUR/0.1-2/Windows-Binary/IPSUR_0.1-2.zip">Windows IPSUR Package</a>
+		<br /><a href="download/IPSUR/0.1-2/MacOSX-Binary/IPSUR_0.1-2.tgz">MacOS X IPSUR Package</a>
+	</div>
+    
+	<p>
+		The current developmental version of the IPSUR package is 0.1-3. Currently the binary package is only available for computers running Windows. This contains very early code for new features and is not suitable for production environments. The developmental version should be used for testing purposes only. 
+	</p>
+    <div align="center">
+		<strong>IPSUR Development Version 0.1-3 </strong>
+		<br /><a href="download/IPSUR/0.1-3/Windows-Binary/IPSUR_0.1-3.zip">Windows IPSUR Package</a>
+	</div>
+    
+	<p>
+		The previous version of the IPSUR package is 0.1-1. The binary package is only available for computers running Windows. While this package will be available, it is recommended that you install the latest released version of the IPSUR package for maximum stability, speed, and features. 
+	</p>
+    <div align="center">
+		<strong>IPSUR Previous Version 0.1-1 </strong>
+		<br /><a href="download/IPSUR/0.1-1/Windows-Binary/IPSUR_0.1-1.zip">Windows IPSUR Package</a>
+	</div>
+    
+	<p>
+		To install the <span class="name">IPSUR</span> binary package, click on the appropriate link below to download the file. Save the package to a convenient location. In Windows:
+		<ol type="1">
+			<li>
+				Open <span class="name">R</span>
+			</li>
+			<li>
+				Select <span class="name">Packages</span>
+			</li>
+			<li>
+				Select <span class="name">Install package(s) from local zip files...</span>
+			</li>
+			<li>
+				Browse and select the downloaded <span class="name">IPSUR</span> package
+			</li>
+		</ol>
+		In MacOS X:
+		<ol type="1">
+			<li>
+				Open <span class="name">R</span>
+			</li>
+			<li>
+				Select <span class="name">Packages &amp; data</span>
+			</li>
+			<li>
+				Select <span class="name">Package Installer</span>
+			</li>
+			<li>
+				From the Package Repository drop down menu select <span class="name">Local Binary Package</span>
+			</li>
+			<li>
+				Click the <span class="name">Install</span> button below
+			</li>
+			<li>
+				Browse and select the downloaded <span class="name">IPSUR</span> package
+			</li>
+		</ol>
+	</p>
+</p>
+</blockquote>
+	
+<div class="articleTitle">Download the IPSUR Source Package</div>
+<blockquote>
+<p>
+	The source code for the <span class="name">IPSUR</span> package is available under the newest version of the GPL.  The source code is recommended for platforms for which package binaries are not made available. It can also be used by developers on all platforms to modify the IPSUR package under the constraints of the GPL. 
+</p>
+	
+<p>
+	The current version of the <span class="name">IPSUR</span> package source is 0.1-2 and is the recommended version. If you are a developer interested in the absolute newest version of the <span class="name">IPSUR</span> packed source is 0.1-3. This package should be considered only be used for developmental purposes. Finally, the previous <span class="name">IPSUR</span> package source, 0.1-1, is also available for those who need it. 
+</p>
+	
+<div align="center">
+	<p>
+		<strong>IPSUR Released Version 0.1-2 Source </strong>
+		<br /><a href="download/IPSUR/0.1-2/source/IPSUR_0.1-2.tar.gz"> IPSUR Package Source </a>
+	</p>
+	<p>
+		<strong>IPSUR Development Version 0.1-3 Source </strong>
+		<br /><a href="download/IPSUR/0.1-3/source/IPSUR_0.1-3.tar.gz"> IPSUR Package Source </a>
+	</p>
+	<p>
+		<strong>IPSUR Previous Version 0.1-2 Source </strong>
+		<br /><a href="download/IPSUR/0.1-1/source/IPSUR_0.1-1.tar.gz"> IPSUR Package Source </a>
+	</p>
+</div>
+	
+<p>Instructions to install from local source code will be coming soon. </p>
+</blockquote>
+<!--END: Page Content-->
 
+<!--BEGIN: Footer-->
+<p align="center">
+	(C) R Foundation, from http://www.r-project.org
+<p align="center">
+<!--END: Footer-->
+  
+		<!--Close main table-->
+		</td>
+	</tr>
+</table>
 </div>
 </body>
 </html>
