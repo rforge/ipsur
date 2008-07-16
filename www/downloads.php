14f7b1bec8262b3949dbd28fddcca5e490775400
gkerns <gkerns@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-16 03:03:55 +0000

    updated the download page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/downloads.php
+++ www/downloads.php
@@ -3,9 +3,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 	<title>IPSUR Project</title>
-	<link rel="Shortcut Icon" href="favicon.ico">
-
-	<style type="text/css">
+	<link rel="Shortcut Icon" href="favicon.ico"><style type="text/css">
 	<!--
 		@import url("main.css");
 		body {
@@ -36,35 +34,41 @@
 <!--END: Top Menu-->
 	
 <!--BEGIN: Page Content-->
-<p class="articleTitle">Download the IPSUR Package</p>
+<p class="articleTitle">
+              <strong>Download the IPSUR Package</strong>
+            </p>
 <blockquote>
 <p>
-	The current released version of the IPSUR package is 0.1-2. Currently the binary packages are only available for computers running Windows and MacOS X. 
-        
-	<div align="center">
-		<strong>IPSUR Released Version 0.1-2 </strong>
-		<br /><a href="download/IPSUR/0.1-2/Windows-Binary/IPSUR_0.1-2.zip">Windows IPSUR Package</a>
-		<br /><a href="download/IPSUR/0.1-2/MacOSX-Binary/IPSUR_0.1-2.tgz">MacOS X IPSUR Package</a>
-	</div>
+                The current released version of the IPSUR package is 0.1-4. There are binary packages available for computers running Windows and MacOS X, and source code for Linux distributions. Downloads for the released version are from CRAN, the Comprehensive R Archive Network. 
+                
     
 	<p>
-		The current developmental version of the IPSUR package is 0.1-3. Currently the binary package is only available for computers running Windows. This contains very early code for new features and is not suitable for production environments. The developmental version should be used for testing purposes only. 
-	</p>
+              </p>
     <div align="center">
-		<strong>IPSUR Development Version 0.1-3 </strong>
-		<br /><a href="download/IPSUR/0.1-3/Windows-Binary/IPSUR_0.1-3.zip">Windows IPSUR Package</a>
+		<strong>IPSUR Released Version 0.1-4 </strong>
+		<br /><a href="http://cran.r-project.org/web/packages/RcmdrPlugin.IPSUR/index.html">CRAN IPSUR Package</a>
 	</div>
     
 	<p>
-		The previous version of the IPSUR package is 0.1-1. The binary package is only available for computers running Windows. While this package will be available, it is recommended that you install the latest released version of the IPSUR package for maximum stability, speed, and features. 
-	</p>
+                <br />
+                 The current development version of the IPSUR package is 0.1-5. This contains very early code for new features and is not suitable for production environments. The development version should be used for testing purposes only. Development downloads are from the project page hosted at R-Forge. </p>
     <div align="center">
-		<strong>IPSUR Previous Version 0.1-1 </strong>
-		<br /><a href="download/IPSUR/0.1-1/Windows-Binary/IPSUR_0.1-1.zip">Windows IPSUR Package</a>
+		<strong>IPSUR Development Version 0.1-5 </strong>
+		<br /><a href="https://r-forge.r-project.org/R/?group_id=58">R-Forge IPSUR Package</a>
 	</div>
     
 	<p>
-		To install the <span class="name">IPSUR</span> binary package, click on the appropriate link below to download the file. Save the package to a convenient location. In Windows:
+		</p>
+
+	</blockquote>
+	<div class="articleTitle">
+              <strong>Installing IPSUR from a Binary Package</strong>
+            </div>
+	<blockquote>
+<p>
+		To install the <span class="name">IPSUR</span> binary package, click on the appropriate link above to download the file. Save the package to a convenient location. 
+	<p>	
+		In Windows:
 		<ol type="1">
 			<li>
 				Open <span class="name">R</span>
@@ -83,54 +87,41 @@
 		<ol type="1">
 			<li>
 				Open <span class="name">R</span>
-			</li>
+                  </li>
 			<li>
 				Select <span class="name">Packages &amp; data</span>
-			</li>
+                  </li>
 			<li>
 				Select <span class="name">Package Installer</span>
-			</li>
+                  </li>
 			<li>
 				From the Package Repository drop down menu select <span class="name">Local Binary Package</span>
-			</li>
+                  </li>
 			<li>
 				Click the <span class="name">Install</span> button below
-			</li>
+                  </li>
 			<li>
 				Browse and select the downloaded <span class="name">IPSUR</span> package
-			</li>
+                  </li>
 		</ol>
 	</p>
 </p>
 </blockquote>
 	
-<div class="articleTitle">Download the IPSUR Source Package</div>
+<div class="articleTitle">
+              <strong>Installing IPSUR from Source</strong>
+            </div>
 <blockquote>
 <p>
-	The source code for the <span class="name">IPSUR</span> package is available under the newest version of the GPL.  The source code is recommended for platforms for which package binaries are not made available. It can also be used by developers on all platforms to modify the IPSUR package under the constraints of the GPL. 
+	The source code for the <span class="name">RcmdrPlugin.IPSUR</span> package is available under the GNU GPL Version 2.  The source code is recommended for platforms for which package binaries are not made available. It can also be used by developers on all platforms to modify <span class="name">IPSUR</span> and redistribute it under the constraints of the GPL. 
 </p>
-	
 <p>
-	The current version of the <span class="name">IPSUR</span> package source is 0.1-2 and is the recommended version. If you are a developer interested in the absolute newest version of the <span class="name">IPSUR</span> packed source is 0.1-3. This package should be considered only be used for developmental purposes. Finally, the previous <span class="name">IPSUR</span> package source, 0.1-1, is also available for those who need it. 
+	Note that in order to install from source, you must have the necessary development tools installed.  In Windows, a good way to get started would be to look at the <span class="name">R Tools</span> page maintained by Duncan Murdoch, which you can find <a href="http://www.murdoch-sutherland.com/Rtools/index.html">here</a>.  Under Ubuntu Linux, this means that you should have the <span class="name">r-base-dev</span> package installed, which you can get with the Synaptic Package Manager. For more information, please see the <a href="http://cran.r-project.org/doc/manuals/R-admin.html">R Installation and Administration Manual</a>.
+</p>	
+	
+<p>For all OSes, the general idea to install from source is to open a terminal window, then <span class="name">cd</span> to the directory in which the source code is saved, and finally issue the command <span class="name">R CMD INSTALL RcmdrPlugin.IPSUR_x.x-x</span>, where <span class="name">x.x-x</span> is the version that you have downloaded.  Given that you have the correct compilation tools installed and that your system is configured correctly, you should have the package installed in a few moments. 
 </p>
-	
-<div align="center">
-	<p>
-		<strong>IPSUR Released Version 0.1-2 Source </strong>
-		<br /><a href="download/IPSUR/0.1-2/source/IPSUR_0.1-2.tar.gz"> IPSUR Package Source </a>
-	</p>
-	<p>
-		<strong>IPSUR Development Version 0.1-3 Source </strong>
-		<br /><a href="download/IPSUR/0.1-3/source/IPSUR_0.1-3.tar.gz"> IPSUR Package Source </a>
-	</p>
-	<p>
-		<strong>IPSUR Previous Version 0.1-2 Source </strong>
-		<br /><a href="download/IPSUR/0.1-1/source/IPSUR_0.1-1.tar.gz"> IPSUR Package Source </a>
-	</p>
-</div>
-	
-<p>Instructions to install from local source code will be coming soon. </p>
-</blockquote>
+</p>
 <!--END: Page Content-->
 
 <!--BEGIN: Footer-->
