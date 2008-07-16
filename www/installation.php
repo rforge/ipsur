38d74f5f332ff5c164475aef779d6ed54f77b213
gkerns <gkerns@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-16 03:14:26 +0000

    fixed the installation.php page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/installation.php
+++ www/installation.php
@@ -72,16 +72,13 @@
 			</ul>
 		</li>
 		<li>
-			<strong>Installing the <span class="name">IPSUR</span> package</strong>: there are several methods for installing the IPSUR package. 
+			<strong>Installing the <span class="name">RcmdrPlugin.IPSUR</span> package</strong>: there are several methods for installing IPSUR: 
                     <br />
 			<br />
 			<ul>
 				<li>
-					<strong>Install from CRAN:</strong> This method works well with Windows and MaxOS X 
-					installations and insures that you have the latest stable version of the package.  To install 
-					directly from CRAN, launch R and type the following at the command prompt ">":
-					
-					<br />
+					<strong>Install from CRAN:</strong> This method works well with Windows and MacOS X installations and insures that you have the latest stable version of the package. To install directly from CRAN, launch R and type the following at the command prompt "&gt;": 
+                        <br />
 					<br />
 					
 					<div align="center">
@@ -93,34 +90,28 @@
 					</div>
 				</li>
 				<li>
-					<strong>Install from download:</strong> Downloads of the binary packages are available on this
-					site for Windows and MacOS X depending on the version. The packages, along with specific 
-					instructions for installing the binary packages on various platforms are available on the 
-					<a href="downloads.php">downloads page</a>. 
+					<strong>Install from download:</strong> Downloads of the binary packages are available for Windows and MacOS X. The packages, along with specific instructions for installing them on various platforms are available on the <a href="downloads.php">downloads page</a>. 
 				</li>
 				<li>
-					<strong>Install from package source:</strong> This method of installation is recommended for
-					all linux operating systems and all other non-supported systems. To install the current 
-					release version of the source,  type the following at the command prompt &quot;&gt;&quot;:
-					  
-					<div class="name" align="center">
+					<strong>Install from package source:</strong> This method of installation is recommended for all linux operating systems and all other non-supported systems. To install the current released version of the source, type the following at the command prompt "&gt;": 
+                        <div class="name" align="center">
 					
 						<br />
-                           install.packages("RcmdrPlugin.IPSUR", contriburl = "http://www.cc.ysu.edu/~gjkerns/IPSUR/package", type="source")
+                           install.packages("RcmdrPlugin.IPSUR", repos = "http://www.cran.r-project.org", type="source")
                         </div>
 					
 					<p>
 						(<strong>note</strong>: you must have the correct compilation tools installed) 
 					</p>
 					<p>
-						Instructions to install from a downloaded copy of the IPSUR source will be coming soon.
+						Instructions to install from a downloaded copy of the IPSUR source are available on the <a href="downloads.php">downloads page</a>.
 					</p>
 				</li>
 			</ul>
 		</li>
 		<li>
-			<strong>Loading the IPSUR package: </strong>Once the IPSUR package is downloaded and installed, it
-			must loaded into R. To do this type the following at the command prompt &quot;&gt;&quot;: 
+			<strong>Loading the RcmdrPlugin.IPSUR package: </strong>Once IPSUR is downloaded and installed, it
+			must be loaded into R. To do this type the following at the command prompt &quot;&gt;&quot;: 
 				
 			<div class="name" align="center">
 			
@@ -130,7 +121,7 @@
 		</li>
 			  
 		<li>
-			<strong>Loading the Dependencies:</strong> When <span class="name">IPSUR</span> loads it will ask 
+			<strong></strong><strong>Installing the Dependencies:</strong> When <span class="name">Rcmdr</span> loads it will ask 
 			you to download a bunch of additional packages. Once this procedure is finished, you will be almost 
 			ready to go.  For more detailed instructions on installing and configuring R and IPSUR you should 
 			next consult the<a href="pdf/Install.pdf"> Installing R and IPSUR</a> document. 
