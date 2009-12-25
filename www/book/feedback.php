5f84573cc938d69073873342819f65690b9b6a7a
gkerns <gkerns@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-25 08:05:06 +0000

    fixed some links, added feedback
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@98 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/book/feedback.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+	<title>IPSUR Project</title>
+	<link rel="Shortcut Icon" href="favicon.ico">
+	<style type="text/css">
+	<!--
+		@import url("main.css");
+		body {
+			background-image: url();
+		}
+	-->
+	</style>
+</head>
+
+<body>
+	<div align="center">
+	<table width="700" height="100%" border="0" cellpadding="0" cellspacing="0">
+		<tr>
+    			<td align="left" valign="top" bgcolor="#FFFFFF">
+			<!-- All of the page content is in this table-->
+
+<!--BEGIN: Page Banner-->
+<?php
+	include "banner.php";
+?>
+<!--END: Page Banner-->
+	
+<!--BEGIN: Top Menu-->
+<?php
+	include "topMenu.php";
+?>
+<!--END: Page Content-->	
+<p class="articleTitle">Giving Feedback</p>
+<blockquote>
+
+<p>
+I would be happy to hear any (most?) comments, suggestions, problems, questions, or requests that you may have about <span class="name">IPSUR</span>. But if you would like to ask/say something, then please ask/say it on the <span class="name">IPSUR</span> mailing lists (or my personal address), and refrain from bogging down the already busy R-help mailing list.  I have made two <span class="name">IPSUR</span> specific mailing lists for this purpose:   
+</p>
+
+<p class="articleTitle">Mailing Lists</p>
+<blockquote>
+<p> 
+  If you have a question about how to use, install, download, or any other operation problem, then I invite you to use the <span class="name">IPSUR</span>-help list at <a href="mailto:ipsur-help@lists.r-forge.org">ipsur-help@lists.r-forge.org</a>.  It is a moderated list, which means that you need to register to be able to post messages freely.  If you are not registered then every message  you send will need to be approved in advance (by me) before it will be posted to the list and stored in the archives. Visit here for registration instructions;  don't worry, it is quick and painless.
+</p>
+<p>
+  Please note that <strong>questions about the exercises</strong> are <strong>intentionally deleted</strong> from the list of invited questions.  I <strong>will not</strong> answer questions about the exercises (or other conceptual matters) unless you <em>happen</em> to catch me on a very, very good day.  You should already have your own professor to teach you probability and statistics and (s)he is the one being paid to do it, not me.
+</p>
+
+<p>
+If you find a mistake in the narrative, if you have a suggestion regarding the subject matter, or if you have <em>constructive</em> criticism of the book, then I invite you to use the <span class="name">IPSUR</span>-devel list at <a href="mailto:ipsur-devel@lists.r-forge.org">ipsur-devel@lists.r-forge.org</a>.  This is also a moderated list;  go here to register.  Before writing please bear in mind that I do pledge to amicably listen, but I do not promise to agree, with you.  Moreover, the fact that <span class="name">IPSUR</span> is FREE means that if you don't like something then you are FREE to fix it yourself.  All that being said, I would be happy to hear about your experiences with the book, good or bad, in the classroom or elsewhere. 
+</p>
+<p>
+If you prefer to write a private message then you can reach me at <a href="mailto:gkerns@ysu.edu">gkerns@ysu.edu</a> (<a href="http://people.ysu.edu/~gkerns">home page</a>).<br />
+</p>
+
+<p>&nbsp;</p>
+</blockquote>
+
+ 
+</blockquote>
+<!--END: Page Content-->
+
+<!--BEGIN: Footer-->      
+ <p align="center">
+	  R logo &copy; the R Foundation, <font face="Courier New"><a href="http://www.r-project.org" style="text-decoration: none"><span style="text-decoration: none">http://www.r-project.org</span></a></font>
+	  </p>
+
+		<!--Close main table-->
+		</td>
+	</tr>
+</table>
+</div>
+</body>
+</html>
