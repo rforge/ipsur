e39221ae4256ff7f087eacd1d83dcd8c90d09081
gkerns <gkerns@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-22 23:18:38 +0000

    small changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/book/downloads.php
+++ www/book/downloads.php
@@ -55,18 +55,18 @@ If you would like to print your own copy of the <strong>published</strong> editi
 </p>
 
 <p>
-If you would like a transparent copy of the published edition of <span class="name">IPSUR</span>, then download the following <span style="text-decoration: none">.zip</span> file.  It contains all of the LaTeX source code, all of the figures (in <span style="text-decoration: none">.pdf</span> and <span style="text-decoration: none">.eps</span>), and the ancillary materials such as <span style="text-decoration: none">IPSUR.R</span> and <span style="text-decoration: none">IPSUR.RData</span>.  This document can be used (in principle) to generate your own personal copy of the published edition of <span class="name">IPSUR</span>, and you are FREE to modify and distribute your own version of it under the constraints of the GNU-FDL.
+If you would like a transparent copy of the published edition of <span class="name">IPSUR</span>, then download the following <span class="name">zip</span> file.  It contains all of the LaTeX source code and all of the figures (in <span class="name">.pdf</span> and <span class="name">.eps</span>).  This document can be used (in principle) to generate your own personal copy of the published edition of <span class="name">IPSUR</span>, and you are FREE to modify and distribute your own version of it under the constraints of the GNU-FDL.
 </p>
 
 <p>
 <div align="center">
-	<strong>LaTeX source, Images, and Ancillaries</strong><br />
+	<strong>LaTeX source and Images</strong><br />
 	<a href="download/IPSUR.zip">IPSUR.zip</a>
 </div>
 </p>
 
 <p>
-Finally, you can download the LyX source file, <span style="text-decoration: none">IPSUR.lyx</span>, which was used by the author to write <span class="name">IPSUR</span>. It is essentially the Sweave source code for the published edition, except that LyX conceals the intermediate step of generating the LaTeX source file and figures.  This one text file will generate a PDF of the entire published edition of <span class="name">IPSUR</span>, with a single click.  Enjoy.  (I certainly have.)
+Finally, you can download the LyX source file, <span class="name">IPSUR.lyx</span>, which was used by the author to write <span class="name">IPSUR</span>. It is essentially the Sweave source code for the published edition, except that LyX conceals the intermediate step of generating the LaTeX source file and figures.  This one text file will generate a PDF of the entire published edition of <span class="name">IPSUR</span>, with a single click.  Enjoy.  (I certainly have.)
 </p>
 
 <p>
@@ -76,7 +76,9 @@ Finally, you can download the LyX source file, <span style="text-decoration: non
 </div>
 </p>
 
-You will also need the <a href="download/IPSUR.bib">BibTeX</a> file.
+<p>
+You will need the <a href="download/IPSUR.bib">BibTeX</a> file to generate the bibliography.
+</p>
 
 <p>
 You are free to print your own copy of the <em>development</em> edition of <span class="name">IPSUR</span>, but I recommend against it for several reasons.  First of all, the electronic copy is in color which does not show up well on black-and-white paper.  Second of all, the electronic copy is optimized for web viewing which means that not all of the fonts are embedded in the PDF - your printer may not render all of the symbols correctly.  Finally, unless you have a very fancy printer you will be printing only on one side of the page - which wastes paper.
