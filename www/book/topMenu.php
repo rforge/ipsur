73306c2db8fbc89bdf57fb561edbb7898808938f
gkerns <gkerns@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-09 03:37:46 +0000

    fixed a dependency issue, updates to the book, and added resources to the webpage
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@186 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/book/topMenu.php
+++ www/book/topMenu.php
@@ -1,10 +1,11 @@
 <div id="navMenu">
   <table width="700" border="0" cellspacing="0" cellpadding="0">
     <tr>
-	<td width="175" align="center" valign="middle"><div align="center"><a href="index.php">Home</a></div></td>
-	<td width="175" align="center" valign="middle"><div align="center"><a href="installation.php">Installation</a></div></td>
-	<td width="175" align="center" valign="middle"><div align="center"><a href="downloads.php">Downloads</a></div></td>
-	<td width="175" align="center" valign="middle"><div align="center"><a href="feedback.php">Feedback</a></div></td>
+	<td widh="140" align="center" valign="middle"><div align="center"><a href="index.php">Home</a></div></td>
+	<td width="140" align="center" valign="middle"><div align="center"><a href="installation.php">Installation</a></div></td>
+	<td width="140" align="center" valign="middle"><div align="center"><a href="downloads.php">Downloads</a></div></td>
+	<td width="140" align="center" valign="middle"><div align="center"><a href="resources.php">Resources</a></div></td>
+	<td width="140" align="center" valign="middle"><div align="center"><a href="feedback.php">Feedback</a></div></td>
     </tr>
   </table>
 </div>
