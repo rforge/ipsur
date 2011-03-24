896435b294214ae8324de3bc833c1222eb50598b
gkerns <gkerns@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-24 17:15:34 +0000

    fixed require("HH") and added boot to SUGGESTS
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@199 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/IPSUR/R/plothtest.R
+++ pkg/IPSUR/R/plothtest.R
@@ -4,7 +4,7 @@
 
 ###################################################
 plot.htest <- function (x, hypoth.or.conf = 'Hypoth',...) { 
-require(HH)
+require("HH")
 if (x$method == "1-sample proportions test with continuity correction" || x$method == "1-sample proportions test without continuity correction"){
   mu <- x$null.value
   obs.mean <- x$estimate
