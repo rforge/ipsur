0b696afa494f3e709d34529ab84318dcf89f24e7
gkerns <gkerns@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-16 14:13:05 +0000

    deleted firstlib and added onattach
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ipsur@201 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RcmdrPlugin.IPSUR/R/First.r
+++ pkg/RcmdrPlugin.IPSUR/R/First.r
@@ -4,15 +4,16 @@
 # can be included in any Rcmdr plug-in package to cause the package to load
 # the Rcmdr if it is not already loaded
 
-.First.lib <- function(libname, pkgname){
-    if (!interactive()) return()
-    Rcmdr <- options()$Rcmdr
-    plugins <- Rcmdr$plugins
-    if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
-        Rcmdr$plugins <- c(plugins, pkgname)
-        options(Rcmdr=Rcmdr)
-        closeCommander(ask=FALSE, ask.save=TRUE)
-        Commander()
-        }
-    }
-    
+
+.onAttach <- function(libname, pkgname){
+       if (!interactive()) return()
+       Rcmdr <- options()$Rcmdr
+       plugins <- Rcmdr$plugins
+       if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
+               Rcmdr$plugins <- c(plugins, pkgname)
+               options(Rcmdr=Rcmdr)
+               closeCommander(ask=FALSE, ask.save=TRUE)
+               Commander()
+       }
+}
+
